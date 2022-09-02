8b6de552b792a682afa1273396dcf91cfd3eaae4
paynah <nanczilla@gmail.com>
2022-09-01 17:04:22 -0700

    Initial commit; installed bootstrap and deleted canned irrelevant code from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <footer className="fixed-bottom credential mb-3 fw-light">
+          <a href="https://github.com/paynah/shecodes-final-project">Open source code</a> by Nancy Yu <br/>
+      </footer>
+      </div>
     </div>
   );
 }
