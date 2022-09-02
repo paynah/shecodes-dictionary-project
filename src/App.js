b01dfacfb82dbfea8cd8c70bf334f8445167ebd9
paynah <nanczilla@gmail.com>
2022-09-02 13:52:03 -0700

    Installed bootstrap, created Dictionary component, added bg img

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
+import Dictionary from "./Dictionary";
+import bgImg from "./images/bg.jpg";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <div className="container">
+    <div className="App" style={{ 
+      backgroundImage: `url(${bgImg})`,
+      height: `100vh`
+    }}>
+        <Dictionary />
+        
         <footer className="fixed-bottom credential mb-3 fw-light">
-          <a href="https://github.com/paynah/shecodes-final-project">Open source code</a> by Nancy Yu <br/>
-      </footer>
-      </div>
+          <a href="https://github.com/paynah/shecodes-dictionary-project">Open source code</a> by Nancy Yu <br/>
+          <a href="https://www.flaticon.com/free-icons/owl" title="owl icons">Owl icons created by cube29 - Flaticon</a>
+        </footer>
     </div>
   );
 }
-
-export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+import owlImg from "./images/owl.png";
+
+export default function Dictionary() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    function onSearchSubmit(event) {
+        event.preventDefault();
+
+        alert(`Searching for ${searchQuery}`);
+    }
+
+    function onSearchQueryChange(event) {
+        setSearchQuery(event.target.value);
+    }
+
+    return (
+        <div className="Dictionary">
+            <img src={owlImg} alt="owl" id="headerImg" />
+            <form onSubmit={onSearchSubmit} id="searchForm">
+                <input type="search" placeholder="Search for a word" onChange={onSearchQueryChange} className="form-control" />
+            </form>
+        </div>
+    );
+}
