78519833aa07e9f0b1f44332d62bc9ed153cb70f
paynah <nanczilla@gmail.com>
2022-09-05 19:56:20 -0700

    Added new ErrorMsg component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,23 +3,28 @@ import "./Dictionary.css";
 import owlImg from "./images/owl.png";
 import axios from "axios";
 import SearchResult from "./SearchResult";
+import ErrorMsg from "./ErrorMsg";
 
 export default function Dictionary() {
     const [searchQuery, setSearchQuery] = useState("");
     const [searchResults, setSearchResults] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
     const baseApiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
     function onDictionarySearchResponse(response) {
+        setErrorMsg(null);
         setSearchResults(response.data);
     }
 
     function handleDictionaryError(response) {
         console.log(response);
         if (typeof response === "object" && response.constructor.name === "AxiosError") {
-            let errorMsg = response.response.data ? response.response.data.message + " " + response.response.data.resolution : response.message;
-            alert(errorMsg);
+            let error = response.response.data ? response.response.data.message + " " + response.response.data.resolution : response.message;
+            setErrorMsg(error);
+            //alert(errorMsg);
         } else {
-            alert("An error has occurred. Please try again.");
+            //alert("An error has occurred. Please try again.");
+            setErrorMsg("An error has occurred. Please try again.");
             console.log(response);
         }
     }
@@ -35,13 +40,27 @@ export default function Dictionary() {
         setSearchQuery(event.target.value);
     }
 
-    return (
-        <div className="Dictionary">
-            <img src={owlImg} alt="owl" id="headerImg" />
-            <form onSubmit={onSearchSubmit} id="searchForm">
-                <input type="search" placeholder="Search for a word" onChange={onSearchQueryChange} className="form-control" />
-            </form>
-            <SearchResult searchResults={searchResults} />
-        </div>
-    );
+    if (errorMsg === null) {
+        return (
+            <div className="Dictionary">
+                <img src={owlImg} alt="owl" id="headerImg" />
+                <form onSubmit={onSearchSubmit} id="searchForm">
+                    <input type="search" placeholder="Search for a word" onChange={onSearchQueryChange} className="form-control" />
+                </form>
+                <SearchResult searchResults={searchResults} />
+            </div>
+        );
+    } else {
+        return (
+            <div className="Dictionary">
+                <img src={owlImg} alt="owl" id="headerImg" />
+                <form onSubmit={onSearchSubmit} id="searchForm">
+                    <input type="search" placeholder="Search for a word" onChange={onSearchQueryChange} className="form-control" />
+                </form>
+                <ErrorMsg message={errorMsg} />
+                <SearchResult searchResults={searchResults} />
+            </div>
+        );
+    }
+    
 }
