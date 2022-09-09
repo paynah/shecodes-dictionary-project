e56acfa4671a06cd1602124c23e58f8d2c38a70a
paynah <nanczilla@gmail.com>
2022-09-08 18:24:19 -0700

    Added photo credit for Pexels

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ export default function App() {
         
         <footer className="credential mb-3 fw-light">
           <a href="https://github.com/paynah/shecodes-dictionary-project">Open source code</a> by Nancy Yu <br/>
+          <a href="https://www.pexels.com">Photos provided by Pexels</a><br/>
           <a href="https://www.flaticon.com/free-icons/owl" title="owl icons">Owl icons created by cube29 - Flaticon</a>
         </footer>
     </div>
