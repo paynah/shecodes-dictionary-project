b624b76a8ffd0fcdbd52f75cae981ef9254dc15b
paynah <nanczilla@gmail.com>
2022-09-08 18:24:26 -0700

    Added new Photos component

--- src/PartOfSpeech.js
+++ src/PartOfSpeech.js
@@ -2,20 +2,21 @@ import React from "react";
 import "./PartOfSpeech.css";
 import Synonyms from "./Synonyms";
 
-export default function PartOfSpeech (props) {
-    return (
-        <section>
-            <div className="PartOfSpeech" key={props.index}>{props.partOfSpeech}
-                {/* Loop through each definition for the current part of speech */}
-                {props.definitions.map((definition, index) => {
-                    return (
-                        <div className="definition" key={index}> 
-                            {index + 1}. {definition.definition} 
-                        </div>
-                    );
-                })}
-                <Synonyms synonyms={props.synonyms} />
+export default function PartOfSpeech(props) {
+  return (
+    <section className="PartOfSpeech">
+      <div key={props.index}>
+        {props.partOfSpeech}
+        {/* Loop through each definition for the current part of speech */}
+        {props.definitions.map((definition, index) => {
+          return (
+            <div className="definition" key={index}>
+              {index + 1}. {definition.definition}
             </div>
-        </section>
-    );
+          );
+        })}
+        <Synonyms synonyms={props.synonyms} />
+      </div>
+    </section>
+  );
 }
